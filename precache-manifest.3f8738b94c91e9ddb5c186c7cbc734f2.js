02dfc40dc461dff8735169ec94bf719bfdb1a754
ballfish <lili668668@gmail.com>
2025-06-13 21:03:02 +0800

    Update version 0.3.1

---
+++ precache-manifest.3f8738b94c91e9ddb5c186c7cbc734f2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "9b59238d0462471fba7a",
+    "url": "/static/js/0.9b59238d.chunk.js"
+  },
+  {
+    "revision": "5bf25c0f4be97319b931",
+    "url": "/static/js/main.5bf25c0f.chunk.js"
+  },
+  {
+    "revision": "4cc86923a5c17173a087",
+    "url": "/static/js/2.4cc86923.chunk.js"
+  },
+  {
+    "revision": "15aa213c8c147aa239e1",
+    "url": "/static/js/3.15aa213c.chunk.js"
+  },
+  {
+    "revision": "9fcd4ed931735bf4f351",
+    "url": "/static/js/4.9fcd4ed9.chunk.js"
+  },
+  {
+    "revision": "966a481604babeebd76d",
+    "url": "/static/js/5.966a4816.chunk.js"
+  },
+  {
+    "revision": "70559bea510800fea099",
+    "url": "/static/js/runtime~main.70559bea.js"
+  },
+  {
+    "revision": "10a42db9f74e3f37996bc5ae09692acb",
+    "url": "/index.html"
+  }
+];
