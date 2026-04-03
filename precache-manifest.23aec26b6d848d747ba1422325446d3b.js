00145eff4b9d01ee8938886c8298ce4d77b0a9d9
ballfish <lili668668@gmail.com>
2026-04-02 18:29:05 -0600

    fix: fix path

---
+++ precache-manifest.23aec26b6d848d747ba1422325446d3b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "5e62d8c188a9ad332e37",
+    "url": "/old-website-2020/static/js/0.5e62d8c1.chunk.js"
+  },
+  {
+    "revision": "53089b737651248767a9",
+    "url": "/old-website-2020/static/js/main.53089b73.chunk.js"
+  },
+  {
+    "revision": "433da2fe8089e2cb8081",
+    "url": "/old-website-2020/static/js/2.433da2fe.chunk.js"
+  },
+  {
+    "revision": "8cea6958f5c0b0a18ea8",
+    "url": "/old-website-2020/static/js/3.8cea6958.chunk.js"
+  },
+  {
+    "revision": "b8a25cb86f9849466436",
+    "url": "/old-website-2020/static/js/4.b8a25cb8.chunk.js"
+  },
+  {
+    "revision": "7d5ab9d153ff93735b10",
+    "url": "/old-website-2020/static/js/5.7d5ab9d1.chunk.js"
+  },
+  {
+    "revision": "0e3e6fd7a0aae4d18363",
+    "url": "/old-website-2020/static/js/runtime~main.0e3e6fd7.js"
+  },
+  {
+    "revision": "a7058c113390dbac12079197837d4798",
+    "url": "/old-website-2020/index.html"
+  }
+];
